16540dc0101338e0572eccf8a73f5494f8e34cd6
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-10 03:00:38 +0900

    Create role for user.

---
+++ src/main/java/io/vanslog/bookstore/role/Role.java
@@ -0,0 +1,29 @@
+package io.vanslog.bookstore.role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private RoleName name;
+}
---
+++ src/main/java/io/vanslog/bookstore/role/RoleName.java
@@ -0,0 +1,6 @@
+package io.vanslog.bookstore.role;
+
+public enum RoleName {
+	ROLE_USER,
+	ROLE_ADMIN
+}
