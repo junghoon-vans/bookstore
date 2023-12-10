064d3d87d8b830e733f302a26f7bbb9b14174cc5
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-10 20:38:46 +0900

    Update data.sql to avoid conflict.

--- src/main/java/io/vanslog/bookstore/role/Role.java
+++ src/main/java/io/vanslog/bookstore/role/Role.java
@@ -8,12 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "roles", uniqueConstraints = { @UniqueConstraint(columnNames = "name") })
 @Getter
 @Setter
 @NoArgsConstructor
