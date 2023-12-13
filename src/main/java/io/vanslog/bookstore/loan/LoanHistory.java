bce9d7bb45e677b55a6be4c43e50f0879d771160
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-13 15:08:37 +0900

    Ignore null property in LoanHistory.

--- src/main/java/io/vanslog/bookstore/loan/LoanHistory.java
+++ src/main/java/io/vanslog/bookstore/loan/LoanHistory.java
@@ -1,7 +1,9 @@
 package io.vanslog.bookstore.loan;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record LoanHistory(Long id, LocalDateTime borrowedAt, LocalDateTime returnedAt) {
 
 	public static LoanHistory from(Loan loan) {
