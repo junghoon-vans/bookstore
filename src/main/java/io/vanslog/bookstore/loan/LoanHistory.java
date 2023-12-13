d2e27d53343038eec28f5fc592dcf78ab06bcdc3
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-13 15:12:43 +0900

    Add username field to LoanHistory.

--- src/main/java/io/vanslog/bookstore/loan/LoanHistory.java
+++ src/main/java/io/vanslog/bookstore/loan/LoanHistory.java
@@ -1,12 +1,14 @@
 package io.vanslog.bookstore.loan;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.vanslog.bookstore.user.User;
 import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record LoanHistory(Long id, LocalDateTime borrowedAt, LocalDateTime returnedAt) {
+public record LoanHistory(Long id, String username, LocalDateTime borrowedAt, LocalDateTime returnedAt) {
 
 	public static LoanHistory from(Loan loan) {
-		return new LoanHistory(loan.getId(), loan.getBorrowedAt(), loan.getReturnedAt());
+		User user = loan.getUser();
+		return new LoanHistory(loan.getId(), user.getUsername(), loan.getBorrowedAt(), loan.getReturnedAt());
 	}
 }
